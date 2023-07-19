10ca9b871557c2337b0dd692d2ca3788b70c3afb
lavruh <lavrsi87@gmail.com>
2023-07-19 08:14:23 +0300

    db selection fixes

--- lib/utils/ui/widgets/db_select_dialog.dart
+++ lib/utils/ui/widgets/db_select_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,17 +9,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 setAppDB(BuildContext context) async {
   final cont = context;
   final pref = Get.find<SharedPreferences>();
-  final f = await FilePicker.platform
-      .pickFiles(dialogTitle: 'Select db', allowedExtensions: ['db']);
+  final f = await FilePicker.platform.saveFile(
+      dialogTitle: 'Select db file to load from or create new db',
+      allowedExtensions: ['db']);
   if (f != null) {
-    final path = f.files.first.path;
-    if (path != null) {
-      pref.setString('dbPath', path);
-      Get.defaultDialog(
-          title: '', middleText: 'To apply changes restart app');
-      if (cont.mounted) {
-        RestartWidget.restartApp(cont);
-      }
+    final path = f;
+    pref.setString('dbPath', path);
+    final file = File(path);
+    if (!file.existsSync()) {
+      file.createSync();
+    }
+    Get.defaultDialog(title: '', middleText: 'To apply changes restart app');
+    if (cont.mounted) {
+      RestartWidget.restartApp(cont);
     }
   }
 }
